df799751bf5d3b4ed68962743d306b3e17a0efec
Noothless <noa.sendlhofer@gmail.com>
2021-11-17 18:17:39 +0100

    Remove old package

--- robot_ws/src/nsra_gxipy/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-d = generate_distutils_setup(
-    packages=['nsra_gxipy'],
-    package_dir={'': 'src'}
-)
-setup(**d)
